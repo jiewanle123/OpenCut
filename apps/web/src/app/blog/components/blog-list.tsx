405c873d9f3e10b99faf82b29b5af537e7ed0fb3
jiewanle123 <jiewanle123@gmail.com>
2026-01-06 04:55:25 +0800

    Fix: Add React import to blog-list component

--- apps/web/src/app/blog/components/blog-list.tsx
+++ apps/web/src/app/blog/components/blog-list.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { Header } from "@/components/header";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
